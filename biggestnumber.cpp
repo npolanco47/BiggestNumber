d15f68b08ac1511e82f7b6036c5d35696c2d4089
npolanco47 <npolanco@mail.csuchico.edu>
2019-09-11 16:51:29 -0700

    New variable input

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,23 +1,31 @@
-//Author:
+//Nixon Polanco
 #include<iostream>
 
 int main()
 {
-  //declare variables here
+ int first = 0;
+ int second = 0;
+//declare variables here
 
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
-
+  std:: cin >> first;
   std::cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
-
+  std::cin >> second;
   //determine if first or second is bigger and store that in a third variable
 
-
-  std::cout<<"Of those two numbers, the biggest is: ";
+  if (first > second)
+  {
+    std::cout<<"Of those two numbers, the biggest is: "<<first;
   //print out the variable representing the biggest number
-
-  std::cout<<std::endl<<"Thank you for playing.\n";
-  
+   }
+      else
+    {
+      std:: cout <<"Of those number, the biggest is: "<<second;
+    }
+ 
+    std::cout<<std::endl<<"Thank you for playing.\n";
+ 
   return 0;
 }
